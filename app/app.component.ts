f40911d03b8436ff6713f3687bd381ffd91392e6
ngirard <girardnatalie@gmail.com>
2016-04-04 19:14:18 +0000

    Add list of contacts based on step 3 of Angular 2 tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,41 @@
 import {Component} from 'angular2/core';
+export class Contact {
+    id: number;
+    identity: string;
+    name: string;
+}
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Contacts';
+    contacts = CONTACTS;
+}
+var CONTACTS: Contact[] = [
+    { "id": 1, "identity": "Batman", "name": "Bruce Wayne" },
+    { "id": 2, "identity": "Spider-Man", "name": "Peter Parker" },
+    { "id": 3, "identity": "Wolverine", "name": "Logan" },
+    { "id": 4, "identity": "Superman", "name": "Clark Kent" },
+    { "id": 5, "identity": "Punisher", "name": "Frank Castle" },
+    { "id": 6, "identity": "Red Robin", "name": "Tim Drake" },
+    { "id": 7, "identity": "Hawkeye", "name": "Clint Barton" },
+    { "id": 8, "identity": "Captain America", "name": "Steve Rogers" },
+    { "id": 9, "identity": "Deadpool", "name": "Wade Wilson" },
+    { "id": 10, "identity": "Thor", "name": "Thor Odinson" },
+    { "id": 11, "identity": "Hulk", "name": "Bruce Banner" },
+    { "id": 12, "identity": "NightWing", "name": "Dick Grayson" },
+    { "id": 13, "identity": "Iron Fist", "name": "Danny Rand" },
+    { "id": 14, "identity": "Iron Man", "name": "Tony Stark" },
+    { "id": 15, "identity": "Nightcrawler", "name": "Kurt Wagner" },
+    { "id": 16, "identity": "Daredevil", "name": "Matt Murdock" },
+    { "id": 17, "identity": "Hellboy", "name": "Anung un Rama" },
+    { "id": 18, "identity": "Green Lantern", "name": "Hal Jordan" },
+    { "id": 19, "identity": "Flash", "name": "Barry Allen" },
+    { "id": 20, "identity": "Swamp Thing", "name": "Dr Alec Holland" },
+    { "id": 21, "identity": "Hellblazer", "name": "John Constantine" },
+    { "id": 22, "identity": "Bucky", "name": "James Buchanan Barnes" },
+    { "id": 23, "identity": "Gambit", "name": "Remy LeBeau" },
+    { "id": 24, "identity": "Red Hood", "name": "Jason Todd" }
+];
